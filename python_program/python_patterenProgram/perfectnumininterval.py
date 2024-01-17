bb90533a4ac22ae35a0be222bcf46c6930085dc4
maheshkshi400 <139653705+maheshkshi400@users.noreply.github.com>
2024-01-17 21:36:21 +0530

    new files added

--- python_program/python_patterenProgram/perfectnumininterval.py
+++ python_program/python_patterenProgram/perfectnumininterval.py
@@ -1,3 +1,10 @@
-num = int(input("enter the number: "))
+lower =int(input("enter the number: "))
+upper =int(input("enter the number: "))
 
-for  i in range()
+for  num in range(lower,upper+1):
+    result=0
+    for i in range(1,num):
+        if num%i==0:
+            result=result+i
+    if num==result:
+        print(num)
