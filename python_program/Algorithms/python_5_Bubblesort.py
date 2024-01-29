904ade82b3174d8dcc3cff4b6af59b7d7e3c14db
maheshkshi400 <139653705+maheshkshi400@users.noreply.github.com>
2024-01-30 01:25:23 +0530

    new files added

--- python_program/Algorithms/python_5_Bubblesort.py
+++ python_program/Algorithms/python_5_Bubblesort.py
@@ -1,11 +1,19 @@
 # Ascending order
 
-list1 =[10,15,4,23,0]
+list1 =[ ]
+num = int(input("How manay numbers you want to enter?: "))
+print("Enter values: ")
+for k in range(num):
+    list1.append(int(input()))
 print("Unsorted list: ",list1)
 for j in range(len(list1)-1):
-    for i in range(len(list1)-1):
+    for i in range(len(list1)-1-j):
         if list1[i] > list1[i+1]:
             list1[i],list1[i+1]=list1[i+1],list1[i]
+            print(list1)
+        else:
+            print(list1)
+    print()
 print("Sorted list: ",list1)
 
 
