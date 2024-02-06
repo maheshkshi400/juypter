c88bdaba8049c5423004c3fbeadc3dde964f8779
maheshkshi400 <139653705+maheshkshi400@users.noreply.github.com>
2024-02-06 10:35:05 +0530

    NEW FILES ADDED

---
+++ R/R_3.R
@@ -0,0 +1,6 @@
+sum<-56+78
+print(sum)
+x<-1:10
+plot(x)
+y<-5:20
+plot(x,y)
---
+++ R/R_4_ifloop.R
@@ -0,0 +1,29 @@
+
+# if statement
+a =10
+if (a>0) {
+   print("positve number")
+}
+
+
+# if_else statement
+b=31
+if(b%%2==0)
+{
+    print("Even number")
+}else {
+   print("odd number")
+}
+
+# If-Else if Ladder
+d=10
+e=50
+g=30
+if ((d>e)&&(d>g)) 
+{
+   cat(d,"is greatest")
+}else if (e>g) {
+   cat(e,"is greatest")
+}else{
+   cat(g,"is greatest")
+}
