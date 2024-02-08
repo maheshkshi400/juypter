828a1bddb4e7fbd2a288db273db54a43c9fe2a58
maheshkshi400 <139653705+maheshkshi400@users.noreply.github.com>
2024-02-08 21:25:01 +0530

    new files added

--- SQL/SQLQuery6-Replace.sql
+++ SQL/SQLQuery6-Replace.sql
@@ -1,1 +1,42 @@
 create table employee2
+(EMPID INT,
+FIRST_NAME VARCHAR(20),
+LAST_NAME VARCHAR(20),
+GENDER VARCHAR(6),
+PHONE INT,
+EMAIL VARCHAR(50),
+ADDRESS VARCHAR(50)
+)
+
+sELECT * FROM employee2;
+
+INSERT INTO  employee2 VALUES
+(1,'Ganesh','Chitale','Male',90807,'gan.chi@gmail.com','12 south calorado')
+
+INSERT INTO  employee2 VALUES
+(2,'Shreya','Gunapure','Female',78960,'shr.gun@gmail.com','13 central calorado')
+
+INSERT INTO  employee2 VALUES
+(3,'Chitra','Tripathi','Female',82805,'chi.tri@gmail.com','18 south  Texas')
+
+INSERT INTO  employee2 VALUES
+(4,'Vaibha','Gadre','Male',67905,'vai.gad@yahoo.com','17 central california')
+
+INSERT INTO  employee2 VALUES
+(5,'Priya','Babar','Female',12345,'pri.bab@gmail.com','18 west texas')
+
+INSERT INTO  employee2 VALUES
+(6,'Sudha','Bhise','Female',43678,'sud.bhi@gmail.com','19 East calorado')
+
+INSERT INTO  employee2 VALUES
+(7,'shirisha','More','Female',43590,'shi.mor@gmail.com','12 East texas')
+
+INSERT INTO  employee2 VALUES
+(8,'Kadmbari','shirvale','Female',89076,'kad.shi@gmail.com','15 south Texas')
+
+INSERT INTO  employee2 VALUES
+(9,'Namrata','Godbole','Female',23456,'nam.god@gmail.com','35 south block calorado')
+
+INSERT INTO  employee2 VALUES
+(10,'Preethi','Sarvate','Female',56790,'pre.sar@gmail.com','19 south  avenue calorado')
+
