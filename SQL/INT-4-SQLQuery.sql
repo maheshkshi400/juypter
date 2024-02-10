d50afa3dd5142de3e5ec7ddf883a406fd6013c33
maheshkshi400 <139653705+maheshkshi400@users.noreply.github.com>
2024-02-10 12:23:37 +0530

    new files changes

---
+++ SQL/INT-4-SQLQuery.sql
@@ -0,0 +1,40 @@
+Create Table Countries
+(
+ Country nvarchar(50),
+ City nvarchar(50)
+)
+GO
+
+SELECT * FROM Countries;
+
+
+Insert into Countries values ('USA','New York')
+Insert into Countries values ('USA','Houston')
+Insert into Countries values ('USA','Dallas')
+
+Insert into Countries values ('India','Hyderabad')
+Insert into Countries values ('India','Bangalore')
+Insert into Countries values ('India','New Delhi')
+
+Insert into Countries values ('UK','London')
+Insert into Countries values ('UK','Birmingham')
+Insert into Countries values ('UK','Manchester')
+
+
+
+
+
+Select Country, City1, City2, City3
+From
+(
+  Select Country, City,
+    'City'+
+      cast(row_number() over(partition by Country order by Country) 
+     as varchar(10)) ColumnSequence
+  from Countries
+) Temp
+pivot
+(
+  max(City)
+  for ColumnSequence in (City1, City2, City3)
+) Piv
