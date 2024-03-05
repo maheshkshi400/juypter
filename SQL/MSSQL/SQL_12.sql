c6ac1a8f3a6e6a923ec0b5fa72bbfecf0709fbbc
maheshkshi400 <maheshkshi400@gmail.com>
2024-03-05 08:30:03 +0530

    new files changes

---
+++ SQL/MSSQL/SQL_12.sql
@@ -0,0 +1,21 @@
+Create Table Inventory(
+ProdName Varchar(20),
+ProductCode Varchar(15),
+Quantity int,
+InventoryDate Date)
+
+
+
+Insert Into Inventory values('Keyboard','K1001',20,'2020-03-01');
+Insert Into Inventory values('Keyboard','K1001',30,'2020-03-02');
+Insert Into Inventory values('Keyboard','K1001',10,'2020-03-03');
+Insert Into Inventory values('Keyboard','K1001',40,'2020-03-04');
+Insert Into Inventory values('Laptop','L1002',100,'2020-03-01');
+Insert Into Inventory values('Laptop','L1002',60,'2020-03-02');
+Insert Into Inventory values('Laptop','L1002',40,'2020-03-03');
+Insert Into Inventory values('Monitor','M5005',30,'2020-03-01');
+Insert Into Inventory values('Monitor','M5005',20,'2020-03-02');
+
+
+--write an sql query to get running total or cumulative sum of quantity for each product code
+
