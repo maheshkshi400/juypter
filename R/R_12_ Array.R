8b8d591a38788bdf49c1786eff8e3b3a39263b60
maheshkshi400 <maheshkshi400@gmail.com>
2024-03-09 19:01:30 +0530

    new files added

--- R/R_12_ Array.R
+++ R/R_12_ Array.R
@@ -1,19 +1,19 @@
-vector1 <- c(6,2,1)
-vector2 <- c(10,16,22,43,15,25)
+vector1 <- c(6, 2, 1)
+vector2 <- c(10, 16, 22, 43, 15, 25)
 
-result <- array(c(vector1,vector2),dim=c(3,3,2))
+result <- array(c(vector1, vector2), dim = c(3, 3, 2))
 print(result)
 
 
-print(result[1,1,1])
+print(result[1, 1, 1])
 
-print(result[3,3,1])
+print(result[3, 3, 1])
 
 
-mat1 <- result[2,2,1]
+mat1 <- result[2, 2, 1]
 print(mat1)
 
-mat2 <- result[3,3,1]
+mat2 <- result[3, 3, 1]
 print(mat2)
 
 print(mat1 + mat2)
