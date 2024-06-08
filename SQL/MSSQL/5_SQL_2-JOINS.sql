3281247e009d989946397c2834eb821bb84dd04f
maheshkshi400 <maheshkshi400@gmail.com>
2024-06-08 17:16:12 +0530

    new chahes added

--- SQL/MSSQL/5_SQL_2-JOINS.sql
+++ SQL/MSSQL/5_SQL_2-JOINS.sql
@@ -44,7 +44,48 @@ DELETE FROM student_3 WHERE id = 5;
 
 DELETE FROM student_3;
 
-SELECT *
+COMMIT;
+
+
+INSERT INTO STUDENT_3 VALUES(1,'Rahul',23,25000);
+INSERT INTO STUDENT_3 VALUES(2, 'Raj', 24, 30000);
+INSERT INTO STUDENT_3 VALUES(3, 'Ramesh', 25, 35000);
+INSERT INTO STUDENT_3 VALUES(4, 'Rakesh', 26, 40000);
+INSERT INTO STUDENT_3 VALUES(5, 'Rajesh', 27, 45000);
+INSERT INTO STUDENT_3 VALUES(5, 'Rajesh', 27, 45000);
+
+SELECT  * FROM student_3 ;
+
+
+SELECT *  FROM STUDENT_3 ORDER BY fees asc;
+
+--duplicate query 
+select id,count(id) from student_3 group by id having(count(id)) >1;
+
+
+--add single colum in table
+ALTER TABLE student_3
+ADD dob date;
+
+-- add multiple columns  
+alter table student_3 add dob date,location varchar(20);
+
+--drop colmn
+ALTER TABLE student_3
+drop column dob;
+
+
+
+
+--sysntax of rename
+EXEC sp_rename 'table_name.old_column_name', 'new_column_name', 'COLUMN';
+
+EXEC sp_rename 'student_3.dob', 'dob1', 'COLUMN';
+
+
+--rename table
+EXEC sp_rename 'student_4','student_3';
+
 
 
 
