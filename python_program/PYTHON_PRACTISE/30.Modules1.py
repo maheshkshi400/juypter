94da77f5134add5f88b011cc7e7a9b17a509c632
maheshkshi400 <maheshkshi400@gmail.com>
2024-07-08 00:23:40 +0530

    add files

--- python_program/PYTHON_PRACTISE/30.Modules1.py
+++ python_program/PYTHON_PRACTISE/30.Modules1.py
@@ -1,2 +1,14 @@
 
 
+from Modules as M  
+import Modules
+from Modules import exponent
+from Modules import Multiplication
+
+
+print(Modules.addition(20,30))
+print(Modules.exponent(2, 10))
+print(Modules.Multiplication(10, 50))
+
+
+
