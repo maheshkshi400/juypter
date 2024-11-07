c2382426e78541574a467ad62360d0dc9c6a2364
maheshkshi400 <maheshkshi400@gmail.com>
2024-11-08 02:29:28 +0530

    save

--- SCALA/1.scala
+++ SCALA/1.scala
@@ -1,2 +1,4 @@
-val x=50
-val result=if(x>20) "Greater" else "Smaller"
+val numbers = List(10, 25, 5, 30, 15)
+val maxNumber = numbers.max
+
+println(s"The maximum number in the list is: $maxNumber")
